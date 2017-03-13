cd7eb9b70cc85d80f2092dc880b35bcbc8d5a9b2
Vladimir Tyalo <bobtial@inbox.ru>
2017-03-13 20:19:05 +0400

    feature(local storage) save redux state in browser local storage

--- src/store.js
+++ src/store.js
@@ -6,7 +6,10 @@ import uuid from 'uuid';
 import { actionType, create } from "./actions";
 import { reducer } from "./reducers";
 const {recipes, activeRecipe, isEditing, ingredients, setFocusId } = reducer;
-const initState = require("./initial-state").store;
+
+const initState = localStorage.getItem("reduxState") ?
+  JSON.parse(localStorage.getItem("reduxState"))
+  : require("./initial-state").store;
 
 
 const App = combineReducers({
@@ -22,7 +25,7 @@ const Reducer = (state = Map({}), action) => {
       let newActive = state.recipes.get(action.id);
       let id = state.activeRecipe.get("id");
 
-      if(!newActive) {
+      if (!newActive) {
         newActive = state.recipes.first();
         id = state.recipes.keys[0];
       }
@@ -34,14 +37,14 @@ const Reducer = (state = Map({}), action) => {
       };
     }
     case actionType.EDIT_RECIPE: {
-       const newRecipe = state.activeRecipe.get("recipe");
-       const title = newRecipe.get("title");
-       if(title == "") return state;
+      const newRecipe = state.activeRecipe.get("recipe");
+      const title = newRecipe.get("title");
+      if (title == "") return state;
 
-       return {
+      return {
         isEditing: false,
         focusId: state.focusId,
-        recipes: state.recipes.merge(fromJS({[action.id]: state.activeRecipe.get("recipe")})),
+        recipes: state.recipes.merge(fromJS({ [action.id]: state.activeRecipe.get("recipe") })),
         activeRecipe: state.activeRecipe
       };
     }
@@ -57,12 +60,13 @@ const Reducer = (state = Map({}), action) => {
         isEditing: true,
         focusId: state.focusId,
         recipes: state.recipes,
-        activeRecipe: fromJS({id: id, recipe: activeRecipe})
+        activeRecipe: fromJS({ id: id, recipe: activeRecipe })
       };
     }
     default: return App(state, action);
   }
 };
+
 
 
 const store = createStore(Reducer,
@@ -74,5 +78,9 @@ const store = createStore(Reducer,
   },
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(fromJS(store.getState()).toJS()));
+});
 
 export default store;
